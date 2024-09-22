e53878aefe18eb7a62184b9829c2150c62abea24
maqlu <b.bartek450@interia.pl>
2024-09-22 16:41:55 +0200

    work fine (i bet)

--- src/main/java/com/example/APIGateway/ApiGatewayApplication.java
+++ src/main/java/com/example/APIGateway/ApiGatewayApplication.java
@@ -2,6 +2,9 @@ package com.example.APIGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -10,4 +13,13 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("library-service",r->r.path("/library/**")
+						.uri("http://localhost:8001/"))
+				.route("test-service",r->r.path("/test/**")
+						.uri("http://localhost:8002/"))
+				.build();
+	}
 }
